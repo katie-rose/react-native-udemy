db234f552f7d930a6615ddffa2f828677ee6bd5c
Katie Rose <katiefelten@gmail.com>
2019-09-10 12:25:35 -0400

    :zap: added image child components

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,8 +1,24 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, View, Button } from "react-native";
 
-const HomeScreen = () => {
-  return <Text style={styles.text}>What it do baybeeee</Text>;
+const HomeScreen = ({ navigation }) => {
+  return (
+    <View>
+      <Text style={styles.text}>What it do baybeeee</Text>
+      <Button
+        onPress={() => navigation.navigate("Components")}
+        title="Go To Components Demo"
+      />
+      <Button
+        title="Go To List Demo"
+        onPress={() => navigation.navigate("List")}
+      />
+      <Button
+        title="Go To Image Demo"
+        onPress={() => navigation.navigate("Image")}
+      />
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
